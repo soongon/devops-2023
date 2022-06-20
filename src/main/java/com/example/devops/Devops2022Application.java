262e6ccec0be9c5a371b8431d19ceb8ba0365451
soongonkim <soongon@hucloud.co.kr>
2022-06-20 14:47:19 +0900

    initial commit

---
+++ src/main/java/com/example/devops/Devops2022Application.java
@@ -0,0 +1,13 @@
+package com.example.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Devops2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devops/Devops2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
